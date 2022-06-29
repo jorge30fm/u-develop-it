3338bcb466900d7b94f6b7b67ee852c99bda8f66
Jorge Felix <jorge30fm@gmail.com>
2022-06-29 16:12:10 -0400

    connect to database

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const mysql = require('mysql2')
 const PORT =  process.env.PORT || 3001;
 const app = express();
+
+//Express middleware
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'jorgeWebDeveloper12484820!?',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`,(err, rows) => {
+    console.log(rows);
+});
 //default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
